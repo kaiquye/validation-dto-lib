d41dc29bd43994cc7be6ce1a8229a75e5eb20b59
kaiquye <kaiquemendesilva82@gmail.com>
2023-03-10 14:38:16 -0300

    chore: remove dist

--- dist/index.d.ts
+++
@@ -1,7 +0,0 @@
-import { DtoBase, ValidationObject, ControllerAdapter } from "./validationObjectTransfer.service";
-declare const _default: {
-    DtoBase: typeof DtoBase;
-    ValidationObject: typeof ValidationObject;
-    ControllerAdapter: typeof ControllerAdapter;
-};
-export default _default;
--- dist/index.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const validationObjectTransfer_service_1 = require("./validationObjectTransfer.service");
-exports.default = { DtoBase: validationObjectTransfer_service_1.DtoBase, ValidationObject: validationObjectTransfer_service_1.ValidationObject, ControllerAdapter: validationObjectTransfer_service_1.ControllerAdapter };
